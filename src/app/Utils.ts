4c7d3604026909cc60911ce780fe80dedc4c6363
Ram kumar <webbieram@outlook.com>
2022-06-22 23:30:26 +0530

    commit original files

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,5 +1,19 @@
+import { UrlWithParsedQuery, parse } from "url";
+
 export class Utils{
+    /* istanbul ignore next */
     public static toUpperCase(str:string):string {
-        return ''
+        if(!str){
+            throw new Error("No Text Provided");
+        }
+        return str.toUpperCase()
+    }
+
+    /* istanbul ignore next */
+    public static parseUrl(url:string): UrlWithParsedQuery {
+        if(!url){
+            throw new Error('No URL Provided');
+        }
+        return parse(url, true)
     }
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,22 @@
-import { Utils } from '../app/Utils'
+import { text } from "stream/consumers";
+import { Utils } from "../app/Utils";
 
-describe('Utils Test suite', ()=> {
-    test('First Test', ()=> {
-        const uppercase = Utils.toUpperCase('ConVeRtme to Uppcase')
-        console.log('First Test')
+describe.skip('Inital Utils Test Suite', ()=> {
+    test.skip('expected output lowercase to uppercse', ()=> {
+        const strngChange = Utils.toUpperCase('abc');
+        expect(strngChange).toBe('ABC');
+    })
+
+    test('URL Parser Test Case', ()=> {
+        const urlString = Utils.parseUrl('http://localhost:9090/login');
+        expect(urlString.href).toBe('http://localhost:9090/login')
+        expect(urlString.port).toBe('9090')
+        expect(urlString.protocol).toBe('http:')
+    })
+})
+
+describe('Console Test', ()=> {
+    test('Test String', ()=> {
+        console.log('Hello from console')
     })
 })
